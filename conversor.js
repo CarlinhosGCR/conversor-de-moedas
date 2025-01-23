ec18816039b995be50283f1fc57e9edde0594411
CarlinhosGCR <carlosanndre.programador@gmail.com>
2025-01-23 09:36:32 -0300

    adição de códigos Js

--- conversor.js
+++ conversor.js
@@ -1,5 +1,15 @@
-function clicarNoBotao(){
+const select = document.getElementById("#select")
+const input = document.getElementsByName("Input-digitável")
+const imgEua = document.getElementsByClassName("estados-unidos")
+const imgEuro = document.getElementsByClassName("euro")
 
+const pDolar = document.getElementsByClassName("dolar")
+const pDolar2 = document.getElementsByClassName("dolar-2")
+
+const pEuro = document.getElementsByClassName("euro")
+const pEuro2 = document.getElementsByClassName("euro-2")
+
+function clicarNoBotao(){
 
     
 }
