1639a1f3c5dcd1d6f8e10d5a2e0c24aa5d1bf30b
CarlinhosGCR <carlosanndre.programador@gmail.com>
2025-01-23 22:27:28 -0300

    Adição de java ao projeto

--- conversor.js
+++ conversor.js
@@ -1,7 +1,10 @@
 const select = document.getElementById("#select")
+const opcao1 = document.getElementsByClassName("opcao1")
+const opcao2 = document.getElementsByClassName("opcao2")
 const input = document.getElementsByName("Input-digitável")
 const imgEua = document.getElementsByClassName("estados-unidos")
 const imgEuro = document.getElementsByClassName("euro")
+
 
 const pDolar = document.getElementsByClassName("dolar")
 const pDolar2 = document.getElementsByClassName("dolar-2")
@@ -9,7 +12,3 @@ const pDolar2 = document.getElementsByClassName("dolar-2")
 const pEuro = document.getElementsByClassName("euro")
 const pEuro2 = document.getElementsByClassName("euro-2")
 
-function clicarNoBotao(){
-
-    
-}
