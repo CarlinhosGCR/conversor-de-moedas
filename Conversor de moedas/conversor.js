1f2b0a508597aea36f6ea93cfe12c03b5c341862
CarlinhosGCR <carlosanndre.programador@gmail.com>
2025-01-24 11:48:33 -0300

    salvando projeto

--- Conversor de moedas/conversor.js
+++ Conversor de moedas/conversor.js
@@ -4,11 +4,8 @@ const opcao2 = document.getElementsByClassName("opcao2")[0]
 const input = document.getElementsByName("Input-digitável")[0]
 const imgEua = document.getElementsByClassName("estados-unidos")[0]
 const imgEuro = document.getElementsByClassName("euro")[0]
-
-
 const pDolar = document.getElementsByClassName("dolar")[0]
 const pDolar2 = document.getElementsByClassName("dolar-2")[0]
-
 const pEuro = document.getElementsByClassName("euro-moeda")[0]
 const pEuro2 = document.getElementsByClassName("euro-moeda-2")[0]
 
